e92c8942197fa8e46d1c9da3df3fa24e9bf46585
Sangjin Cho <sangjin3901@gmail.com>
2012-11-21 01:28:32 +0900

    0.7d - ext4 and workqueue and fifo etc..

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 32 */
+/* This file is auto generated, version 34 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#32 SMP PREEMPT Tue Nov 20 23:51:18 KST 2012"
+#define UTS_VERSION "#34 SMP PREEMPT Wed Nov 21 01:22:40 KST 2012"
 #define LINUX_COMPILE_BY "sjkoon"
 #define LINUX_COMPILE_HOST "LongDH"
 #define LINUX_COMPILER "gcc version 4.7.3 20121001 (prerelease) (crosstool-NG linaro-1.13.1-4.7-2012.10-20121022 - Linaro GCC 2012.10) "
